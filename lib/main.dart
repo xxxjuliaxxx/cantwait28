5e93ee6fd3f1983990642cf7f541548dd41f85f1
Julia Czaplejewicz <julakasia14@gmail.com>
2022-09-18 19:35:04 +0200

    Code review

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:cantwait28/features/home/pages/home_page.dart';
 import 'package:cantwait28/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
