6f57bda6771a956ef346396de1ebe21789688f17
Julia Czaplejewicz <julakasia14@gmail.com>
2022-09-22 22:37:45 +0200

    Implement item Model

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:cantwait28/models/item_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 part 'home_state.dart';
 
@@ -15,7 +16,15 @@ class HomeCubit extends Cubit<HomeState> {
         .snapshots()
         .listen(
       (items) {
-        emit(HomeState(items: items));
+        final itemModels = items.docs.map((doc) {
+          return ItemModel(
+            id: doc.id,
+            title: doc['title'],
+            imageURL: doc['image_url'],
+            releaseDate: (doc['release_date'] as Timestamp).toDate(),
+          );
+        }).toList();
+        emit(HomeState(items: itemModels));
       },
     )..onError(
         (error) {
